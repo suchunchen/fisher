b658945d141d5158f97cebe807f4666373a55fdd
suchunchen <87444109@qq.com>
2019-05-09 18:05:42 +0800

    完成list获取，视频获取的功能

--- search.go
+++ search.go
@@ -8,8 +8,33 @@ import (
 	"net/http"
 	"net/url"
 	"regexp"
+	"strings"
 	"time"
 )
+
+func httpGet(requestUrl string) (response string, header map[string][]string) {
+	if strings.HasPrefix(requestUrl, "//") {
+		requestUrl = "https:" + requestUrl
+	}
+
+	spew.WDump(requestUrl)
+	resp, err := http.Get(requestUrl)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if loc, ok := resp.Header["Location"]; ok {
+		return httpGet(loc[0])
+	}
+
+	if body, err := ioutil.ReadAll(resp.Body); err != nil {
+		panic(err)
+	} else {
+		return string(body), resp.Header
+	}
+}
 
 func search(keyword string) {
 	// https://s.taobao.com/search?q=%E5%A5%B3%E8%A3%85&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20190509&ie=utf8
@@ -23,50 +48,80 @@ func search(keyword string) {
 
 	requestUrl := "https://s.taobao.com/search?" + u.Encode()
 
-	spew.WDump(requestUrl)
-
-	resp, err := http.Get(requestUrl)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
 	// 返回的请求体 resp.Header 中以下2个字段有何作用，待研究
 	// Eagleeye-Traceid: 0b838cef15573910940247287e4d06
 	// Set-Cookie: JSESSIONID=7CC8CEAFCE14ACD185488BCF4A5F650B; Path=/; HttpOnly
-	if body, err := ioutil.ReadAll(resp.Body); err != nil {
-		panic(err)
-	} else {
-		// spew.WDump(string(body))
-		re := regexp.MustCompile("g_page_config = ({.*});")
+	response, _ := httpGet(requestUrl)
 
-		matches := re.FindAllStringSubmatch(string(body), -1)
-
-		if len(matches) != 1 || len(matches[0]) != 2 {
-			spew.WDump("没找到")
-			return
-		}
-
-		itemlist := gjson.Get(matches[0][1], "mods.itemlist.data.auctions")
-
-		if itemlist.String() == "" {
-			spew.WDump("没有商品信息")
-			return
-		}
-
-		var detailUrlList []string
-
-		// 获取每个搜索页的detailUrlList
-		itemlist.ForEach(func(key, item gjson.Result) bool {
-			detailUrlList = append(detailUrlList, item.Get("detail_url").String())
-			return true
-		})
-
-		spew.WDump(detailUrlList)
+	matches := regexp.MustCompile("g_page_config = ({.*});").FindAllStringSubmatch(response, -1)
+	if len(matches) != 1 || len(matches[0]) != 2 {
+		spew.WDump("没找到")
+		return
 	}
 
+	itemlist := gjson.Get(matches[0][1], "mods.itemlist.data.auctions")
+
+	if itemlist.String() == "" {
+		spew.WDump("没有商品信息")
+		return
+	}
+
+	var detailUrlList []string
+
+	// 获取每个搜索页的detailUrlList
+	itemlist.ForEach(func(index, item gjson.Result) bool {
+		detailUrlList = append(detailUrlList, item.Get("detail_url").String())
+
+		detailContent, _ := httpGet(item.Get("detail_url").String())
+		getDetailVideo(detailContent)
+
+		return true
+	})
+
+	spew.WDump(detailUrlList)
+}
+
+// 获取详情页的视频链接
+func getDetailVideo(content string) string {
+	// 格式1：
+	// //cloud.video.taobao.com/play/u/3351172141/p/1/e/1/t/8/218965885328.swf
+	matches := regexp.MustCompile("//cloud.video.taobao.com/play/u/(\\d+)/p/1/e/1/t/8/(\\d+).swf").FindAllStringSubmatch(content, -1)
+
+	var videoId, videoOwnerId string
+
+	if len(matches) == 1 && len(matches[0]) == 3 {
+		videoOwnerId = matches[0][1]
+		videoId = matches[0][2]
+	} else {
+		// 格式2
+		// Hub.config.set('video', {"videoDuaration":"30","picUrl":"//img.alicdn.com/imgextra/i2/334724657/O1CN011kGwwXENhmxmvD7_!!334724657.jpg_310x310.jpg","videoId":"50254186813","autoplay":"1","videoOwnerId":"334724657","videoStatus":"0"})
+		videoOwnerId = getInfoFromJson("videoOwnerId", content)
+		videoId = getInfoFromJson("videoId", content)
+
+		if videoOwnerId == "" || videoId == "" {
+			return ""
+		}
+	}
+
+	return "https://cloud.video.taobao.com/play/u/" + videoOwnerId + "/p/1/e/6/t/1/" + videoId + ".mp4"
+}
+
+func getInfoFromJson(key, content string) string {
+	matches := regexp.MustCompile("\""+key+"\":\"(\\d+)\"").FindAllStringSubmatch(content, -1)
+
+	if len(matches) == 1 && len(matches[0]) == 2 {
+		return matches[0][1]
+	}
+	return ""
 }
 
 func main() {
-	search("女装")
+	// search("女装")
+
+	// 格式1
+	detailContent, _ := httpGet("https://detail.tmall.com/item.htm?id=534714077614&ali_trackid=2:mm_26632614_0_0:1556445756_275_1897617264&spm=a21bo.7925826.192013.1.62504c0d2Hbyvd")
+
+	// 格式2
+	// detailContent, _ := httpGet("https://item.taobao.com/item.htm?spm=a217f.8051907.312171.37.7c4c3308ygq3wn&id=576633151569&qq-pf-to=pcqq.c2c")
+	spew.WDump(getDetailVideo(detailContent))
 }
